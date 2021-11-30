a2134c82a5d1e4065cfc495d27f0051e6f0ef9a1
Sampaio Leal <samp14hd@gmail.com>
2021-11-30 17:06:33 -0300

    feat: exposing useFormixContext

--- src/formixContext.ts
+++ src/formixContext.ts
@@ -4,6 +4,12 @@ import FormixStore from './FormixStore';
 // TODO: improve types
 export const FormContext = createContext<FormixStore<any> | null>(null);
 
+/**
+ * A hook to access the FormixStore from a React component.
+ *
+ * Only works if the component is a child of a Formix.
+ * @returns A FormixStore instance
+ */
 export function useFormixContext() {
   const context = useContext(FormContext);
 
