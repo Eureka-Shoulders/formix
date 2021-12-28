d5e0ef4a09460bbcef3a07053e249fb5aca255a4
Sampaio Leal <samp14hd@gmail.com>
2021-12-28 14:22:01 -0300

    fix: lib export standard to cjs

--- scripts/build.js
+++ scripts/build.js
@@ -30,6 +30,25 @@ const inputs = getInputs(sourceFolder);
 rimraf(distFolder, (err) => {
   if (err) console.error(err);
 
+  console.time('Generating CJS output...');
+  esbuild.buildSync({
+    entryPoints: [...inputs],
+    format: 'cjs',
+    outbase: sourceFolder,
+    outdir: distFolder,
+    jsx: 'transform',
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+    target: 'es6',
+    loader: {
+      '.json': 'json',
+    },
+    tsconfig: 'tsconfig.json',
+    minify: true,
+    inject: ['./scripts/react-shim.js'],
+  });
+  console.timeEnd('Generating CJS output...');
+
   console.time('Generating ESM output...');
   esbuild.buildSync({
     entryPoints: [...inputs],
@@ -49,22 +68,8 @@ rimraf(distFolder, (err) => {
   });
   console.timeEnd('Generating ESM output...');
 
-  console.time('Generating CJS output...');
-  esbuild.buildSync({
-    entryPoints: [...inputs],
-    format: 'cjs',
-    outbase: sourceFolder,
-    outdir: distFolder + '/cjs',
-    jsx: 'transform',
-    jsxFactory: 'React.createElement',
-    jsxFragment: 'React.Fragment',
-    target: 'es6',
-    loader: {
-      '.json': 'json',
-    },
-    tsconfig: 'tsconfig.json',
-    minify: true,
-    inject: ['./scripts/react-shim.js'],
-  });
-  console.timeEnd('Generating CJS output...');
+  fs.copyFileSync(
+    path.join('./package.json'),
+    path.join(distFolder, './package.json')
+  );
 });
